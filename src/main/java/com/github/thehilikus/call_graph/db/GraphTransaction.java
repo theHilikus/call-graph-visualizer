0c330f83b05917a24998e1371d1a475e204db302
Alejandro Endo <theHilikus@gmail.com>
2025-03-30 23:56:20 -0400

    Create target call node and call relationship

--- src/main/java/com/github/thehilikus/call_graph/db/GraphTransaction.java
+++ src/main/java/com/github/thehilikus/call_graph/db/GraphTransaction.java
@@ -1,64 +1,88 @@
 package com.github.thehilikus.call_graph.db;
 
-import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * POJO to represent a transaction in the database
  */
 public class GraphTransaction implements AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(GraphTransaction.class);
-    private final Transaction transaction;
-    private final Collection<String> newNodes = new HashSet<>();
+    private static final String METHOD_LABEL = "Method";
+    private static final String FQN = "fullQualifiedClassName";
+    private static final String SIMPLE_NAME = "simpleClassName";
+    private static final String SIGNATURE = "signature";
+    private static final String STATIC = "static";
+    private final Transaction neoTx;
+    private final Map<String, Node> newNodes = new HashMap<>();
 
     GraphTransaction(Transaction transaction) {
         LOG.info("Starting DB transaction");
-        this.transaction = transaction;
+        this.neoTx = transaction;
     }
 
-    public void addNode(String className, String methodSignature) {
-        if (transaction == null) {
-            throw new GraphDatabaseException("Transaction not started");
-        }
+    public Node addMethodNode(String className, String methodSignature, boolean isStatic) {
+        throwIfNoTransaction();
         LOG.trace("Creating node for method {}#{}", className, methodSignature);
 
-        Node node = transaction.createNode(Label.label(methodSignature));
-        node.setProperty("class", className);
-        newNodes.add(className + "#" + methodSignature);
+        Node node = neoTx.createNode(Label.label(METHOD_LABEL));
+        node.setProperty(FQN, className);
+        node.setProperty(SIMPLE_NAME, className.substring(className.lastIndexOf(".") + 1));
+        node.setProperty(SIGNATURE, methodSignature);
+        node.setProperty(STATIC, isStatic);
+        newNodes.put(className + "#" + methodSignature, node);
+
+        return node;
     }
 
-    public boolean containsNode(String className, String methodSignature) {
-        return newNodes.contains(className + "#" + methodSignature);
+    private void throwIfNoTransaction() {
+        if (neoTx == null) {
+            throw new GraphDatabaseException("Transaction not started");
+        }
+    }
+
+    public boolean containsMethodNode(String className, String methodSignature) {
+        return newNodes.containsKey(className + "#" + methodSignature);
+    }
+
+
+    public Node getMethodNode(String className, String methodSignature) {
+        return newNodes.get(className + "#" + methodSignature);
     }
 
     public void commit() {
-        if (transaction != null) {
-            LOG.info("Committing DB transaction");
-            transaction.commit();
-        } else {
-            throw new GraphDatabaseException("Transaction not started");
-        }
+        throwIfNoTransaction();
+
+        LOG.info("Committing DB transaction");
+        neoTx.commit();
     }
 
     public void rollback() {
-        if (transaction != null) {
-            LOG.info("Rolling back DB transaction");
-            transaction.rollback();
-        } else {
-            throw new GraphDatabaseException("Transaction not started");
-        }
+        throwIfNoTransaction();
+
+        LOG.info("Rolling back DB transaction");
+        neoTx.rollback();
+    }
+
+    public void addRelationship(String relationshipName, Node currentNode, Node targetNode) {
+        throwIfNoTransaction();
+        LOG.trace("Creating relationship {} between {}#{} and {}#{}", relationshipName, currentNode.getProperty(SIMPLE_NAME), currentNode.getProperty(SIGNATURE), targetNode.getProperty(SIMPLE_NAME), targetNode.getProperty(SIGNATURE));
+        currentNode.createRelationshipTo(targetNode, RelationshipType.withName(relationshipName));
+    }
+
+    public int getNodeCount() {
+        throwIfNoTransaction();
+        return newNodes.size();
     }
 
     @Override
     public void close() {
-        if (transaction != null) {
-            transaction.close();
+        if (neoTx != null) {
+            neoTx.close();
         }
     }
 }
--- src/main/java/com/github/thehilikus/call_graph/jar/MethodAnalyzer.java
+++ src/main/java/com/github/thehilikus/call_graph/jar/MethodAnalyzer.java
@@ -1,6 +1,7 @@
 package com.github.thehilikus.call_graph.jar;
 
 import com.github.thehilikus.call_graph.db.GraphTransaction;
+import org.neo4j.graphdb.Node;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.MethodNode;
@@ -10,10 +11,12 @@ import org.objectweb.asm.tree.MethodNode;
  */
 public class MethodAnalyzer extends MethodVisitor {
     private static final String CONSTRUCTOR_NAME = "<init>";
+    private static final String CALLS = "Calls";
     private final String className;
     private final GraphTransaction activeTransaction;
     private final String description;
     private final String methodName;
+    private Node currentNode;
 
     protected MethodAnalyzer(int api, MethodVisitor methodVisitor, String className, MethodNode methodNode, GraphTransaction tx) {
         super(api, methodVisitor);
@@ -23,15 +26,52 @@ public class MethodAnalyzer extends MethodVisitor {
         this.activeTransaction = tx;
     }
 
-    private String getCleanName(String rawName) {
-        String result = rawName;
-        if (CONSTRUCTOR_NAME.equals(rawName)) {
+    /**
+     * Called once for the current method
+     */
+    @Override
+    public void visitCode() {
+        String signature = cleanMethodName(className, methodName) + buildArgumentsList();
+        currentNode = addOrGetNode(className, signature);
+
+        super.visitCode();
+    }
+
+    /**
+     * Called for every call of the current method
+     */
+    @Override
+    public void visitMethodInsn(int opcode, String targetClassRaw, String targetMethodNameRaw, String descriptor, boolean isInterface) {
+        if (currentNode == null) {
+            throw new JarAnalysisException("Unknown current method with target method" + targetClassRaw + "#" + targetMethodNameRaw);
+        }
+        String targetClass = targetClassRaw.replace("/", ".");
+        String targetMethod = cleanMethodName(targetClass, targetMethodNameRaw) + buildArgumentsList();
+        Node targetNode = addOrGetNode(targetClass, targetMethod);
+        activeTransaction.addRelationship(CALLS, currentNode, targetNode);
+
+        super.visitMethodInsn(opcode, targetClassRaw, targetMethodNameRaw, descriptor, isInterface);
+    }
+
+    private Node addOrGetNode(String targetClass, String targetMethod) {
+        Node result;
+        if (!activeTransaction.containsMethodNode(targetClass, targetMethod)) {
+            result = activeTransaction.addMethodNode(targetClass, targetMethod, false); //TODO: fix isStatic
+        } else {
+            result = activeTransaction.getMethodNode(targetClass, targetMethod);
+        }
+        return result;
+    }
+
+    private String cleanMethodName(String className, String rawMethodName) {
+        String result = rawMethodName;
+        if (CONSTRUCTOR_NAME.equals(rawMethodName)) {
             result = className.substring(className.lastIndexOf('.') + 1);
         }
         return result;
     }
 
-    private String getCleanArguments() {
+    private String buildArgumentsList() {
         StringBuilder result = new StringBuilder("(");
         Type[] argumentTypes = Type.getArgumentTypes(description);
         for (int i = 0; i < argumentTypes.length; i++) {
@@ -43,21 +83,5 @@ public class MethodAnalyzer extends MethodVisitor {
         result.append(")");
 
         return result.toString();
-    }
-
-    @Override
-    public void visitCode() {
-        String signature = getCleanName(methodName) + getCleanArguments();
-        if (activeTransaction.containsNode(className, signature)) {
-            return;
-        }
-
-        activeTransaction.addNode(className, signature);
-        super.visitCode();
-    }
-
-    @Override
-    public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
-        super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
     }
 }
