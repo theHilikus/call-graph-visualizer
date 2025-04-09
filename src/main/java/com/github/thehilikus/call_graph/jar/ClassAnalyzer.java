73a05b4f03004ee98cb297452731da2b325e972b
Alejandro Endo <theHilikus@gmail.com>
2025-04-08 22:17:19 -0400

    Implement include/exclude logic in class analyzer

--- src/main/java/com/github/thehilikus/call_graph/jar/ClassAnalyzer.java
+++ src/main/java/com/github/thehilikus/call_graph/jar/ClassAnalyzer.java
@@ -3,6 +3,7 @@ package com.github.thehilikus.call_graph.jar;
 import com.github.thehilikus.call_graph.db.GraphConstants;
 import com.github.thehilikus.call_graph.db.GraphConstants.Classes;
 import com.github.thehilikus.call_graph.db.GraphTransaction;
+import com.github.thehilikus.call_graph.run.Filter;
 import org.neo4j.graphdb.Node;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
@@ -22,24 +23,48 @@ public class ClassAnalyzer extends ClassVisitor {
     private final String className;
     private final Node jarNode;
     private final GraphTransaction activeTransaction;
+    private final Filter classFilter;
     private Node currentNode;
 
 
-    protected ClassAnalyzer(String className, Node jarNode, GraphTransaction tx) {
+    protected ClassAnalyzer(String className, Node jarNode, GraphTransaction tx, Filter classFilter) {
         super(Opcodes.ASM9);
         this.className = className;
         this.jarNode = jarNode;
         this.activeTransaction = tx;
+        this.classFilter = classFilter;
     }
 
     @Override
     public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-        currentNode = createClassNode();
+        if (isIncluded(className, classFilter)) {
+            currentNode = createClassNode();
 
-        LOG.trace("Creating relationship '{}' between {} and {}", GraphConstants.Relations.ARCHIVES, jarNode.getProperty(GraphConstants.ID), currentNode.getProperty(GraphConstants.ID));
-        activeTransaction.addRelationship(GraphConstants.Relations.ARCHIVES, jarNode, currentNode);
+            LOG.trace("Creating relationship '{}' between {} and {}", GraphConstants.Relations.ARCHIVES, jarNode.getProperty(GraphConstants.ID), currentNode.getProperty(GraphConstants.ID));
+            activeTransaction.addRelationship(GraphConstants.Relations.ARCHIVES, jarNode, currentNode); //jar to class
+        }
 
         super.visit(version, access, name, signature, superName, interfaces);
+    }
+
+    static boolean isIncluded(String className, Filter classFilter) {
+        for (String prefix : classFilter.exclude()) {
+            if (className.startsWith(prefix)) {
+                LOG.trace("Excluding class {}", className);
+                return false;
+            }
+        }
+        if (classFilter.include() != null) {
+            for (String prefix : classFilter.include()) {
+                if (className.startsWith(prefix)) {
+                    LOG.trace("Including class {}", className);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        return true;
     }
 
     private Node createClassNode() {
@@ -47,12 +72,14 @@ public class ClassAnalyzer extends ClassVisitor {
                 GraphConstants.ID, className,
                 Classes.SIMPLE_NAME, className.substring(className.lastIndexOf('.') + 1)
         );
-        LOG.debug("Creating node for class {}", className);
         return activeTransaction.addNode(className, Classes.CLASS_LABEL, properties);
     }
 
     @Override
     public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+        if (currentNode == null) {
+            return null;
+        }
         MethodNode methodNode = new MethodNode(api, access, name, descriptor, signature, exceptions);
         return new MethodAnalyzer(api, super.visitMethod(access, name, descriptor, signature, exceptions), currentNode, methodNode, activeTransaction);
     }
